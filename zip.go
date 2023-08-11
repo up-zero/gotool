484e919dda7882862c6c7b7bc471a74eb84a3322
GetcharZp <2272795814@qq.com>
2023-08-11 10:34:16 +0800

    feat unzip

--- zip.go
+++ zip.go
@@ -2,11 +2,17 @@ package gotool
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+type UnzipNotify struct {
+	Progress int    `json:"progress"` // 解压进度
+	FPath    string `json:"f_path"`   // 解压文件的路径
+}
 
 // Zip 文件夹压缩
 //
@@ -18,8 +24,8 @@ func Zip(dest, src string) error {
 
 // ZipWithNotify 带通知的文件夹压缩
 //
-// destZip 压缩到的文件 例如：/var/xxx.zip
-// srcFile 源文件夹 例如：/var/xxx
+// dest 压缩到的文件 例如：/var/xxx.zip
+// src 源文件夹 例如：/var/xxx
 // ch 用于通知压缩进度
 func ZipWithNotify(dest, src string, ch chan int) error {
 	zipFile, err := os.Create(dest)
@@ -66,4 +72,75 @@ func ZipWithNotify(dest, src string, ch chan int) error {
 		return err
 	})
 	return err
+}
+
+// Unzip 文件解压
+//
+// dest 解压到的路径 例如：/var/xxx
+// src 文件路径 例如：/var/xxx.zip
+func Unzip(dest, src string) error {
+	return UnzipWithNotify(dest, src, nil)
+}
+
+// UnzipWithNotify 带通知的文件解压
+//
+// dest 解压到的路径 例如：/var/xxx
+// src 文件路径 例如：/var/xxx.zip
+// ch 用于通知解压进度
+func UnzipWithNotify(dest, src string, ch chan *UnzipNotify) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	index := 0
+
+	for _, f := range r.File {
+		fpath := filepath.Join(dest, f.Name)
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path : %s", fpath)
+		}
+		if f.FileInfo().IsDir() {
+			if err = os.MkdirAll(fpath, os.ModePerm); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// 创建父级文件夹
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return err
+		}
+
+		// 打开输出文件
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+
+		// 打开输入文件
+		inFile, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		// 写入文件
+		_, err = io.Copy(outFile, inFile)
+		outFile.Close()
+		inFile.Close()
+		if err != nil {
+			return err
+		}
+
+		// 通知解压进度
+		if ch != nil {
+			un := &UnzipNotify{
+				Progress: index,
+				FPath:    fpath,
+			}
+			ch <- un
+			index++
+		}
+	}
+	return nil
 }
--- zip_test.go
+++ zip_test.go
@@ -1,6 +1,8 @@
 package gotool
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestZip(t *testing.T) {
 	t.Log(Zip("E:\\github\\up-zero\\gotool\\tmp\\dir.zip", "E:\\github\\up-zero\\gotool\\tmp\\a"))
@@ -24,4 +26,31 @@ func TestZipWithNotify(t *testing.T) {
 		}
 	}()
 	t.Log(ZipWithNotify(dest, src, ch))
+}
+
+func TestUnzip(t *testing.T) {
+	t.Log(Unzip("E:\\github\\up-zero\\gotool\\tmp\\b", "E:\\github\\up-zero\\gotool\\tmp\\dir.zip"))
+}
+
+func TestUnzipWithNotify(t *testing.T) {
+	src := "E:\\github\\up-zero\\gotool\\tmp\\dir.zip"
+	dest := "E:\\github\\up-zero\\gotool\\tmp\\b"
+	ch := make(chan *UnzipNotify)
+	defer close(ch)
+	finish := make(chan struct{})
+	defer close(finish)
+	go func() {
+		for {
+			select {
+			case data := <-ch:
+				t.Log("data => ", data)
+			case <-finish:
+				return
+			}
+		}
+	}()
+	if err := UnzipWithNotify(dest, src, ch); err != nil {
+		t.Fatal(err)
+	}
+	finish <- struct{}{}
 }
