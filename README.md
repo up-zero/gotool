e61f756b7bb1ce9ee3c56ec480ccd509580cd878
GetcharZp <2272795814@qq.com>
2023-08-14 18:26:46 +0800

    feat 带通知的运行命令

--- README.md
+++ README.md
@@ -40,5 +40,7 @@ func main() {
 + **ZipWithNotify** 带通知的文件夹压缩
 + **Unzip** 文件解压
 + **UnzipWithNotify** 带通知的文件解压
-+ **RunShell** 运行shell命令或脚本
-+ **RunCommand** 运行命令
++ **ExecShell** 运行shell命令或脚本
++ **ExecCommand** 运行命令
++ **ExecShellWithNotify** 带通知的运行shell命令或脚本
++ **ExecCommandWithNotify** 带通知的运行命令
--- exec.go
+++ exec.go
@@ -3,30 +3,32 @@ package gotool
 import (
 	"bufio"
 	"bytes"
-	"log"
 	"os/exec"
 )
 
-// RunShell 运行shell命令或脚本
-//
-// shell: shell 命令或脚本
-func RunShell(shell string) error {
-	cmd := exec.Command("/bin/bash", "-c", shell)
-
-	return runCmd(cmd)
-}
-
-// RunCommand 运行命令
+// ExecCommand 运行命令
 //
 // name: 命令名称
 // arg: 命令参数
-func RunCommand(name string, arg ...string) error {
+func ExecCommand(name string, arg ...string) error {
 	cmd := exec.Command(name, arg...)
-
-	return runCmd(cmd)
+	return execCmd(cmd, nil)
 }
 
-func runCmd(cmd *exec.Cmd) error {
+// ExecCommandWithNotify 带通知的运行命令
+//
+// name: 命令名称
+// arg: 命令参数
+func ExecCommandWithNotify(ch chan string, name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+	return execCmd(cmd, ch)
+}
+
+// execCmd 执行命令
+//
+// cmd: 命令
+// ch: 输出通道
+func execCmd(cmd *exec.Cmd, ch chan string) error {
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 	stdout, err := cmd.StdoutPipe()
@@ -40,15 +42,21 @@ func runCmd(cmd *exec.Cmd) error {
 	scanner := bufio.NewScanner(stdout)
 	for scanner.Scan() {
 		text := scanner.Text()
-		log.Println(text)
+		if ch != nil {
+			ch <- text
+		}
 	}
 	if err := scanner.Err(); err != nil {
-		log.Println(err)
+		if ch != nil {
+			ch <- err.Error()
+		}
 		return err
 	}
 
 	if err := cmd.Wait(); err != nil {
-		log.Println(stderr.String())
+		if ch != nil {
+			ch <- stderr.String()
+		}
 		return err
 	}
 	return nil
