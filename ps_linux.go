e417df14766a05d5abb1b90616c9dae4f2238493
GetcharZp <2272795814@qq.com>
2023-10-26 16:12:13 +0800

    fix linux PsByName find by /proc

--- ps_linux.go
+++ ps_linux.go
@@ -3,8 +3,9 @@
 package gotool
 
 import (
-	"os/exec"
-	"regexp"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"strings"
 )
 
@@ -12,40 +13,53 @@ import (
 //
 // name: 程序名
 func PsByName(name string) ([]Process, error) {
-	ch := make(chan string)
-	defer close(ch)
-	finish := make(chan struct{})
-	defer close(finish)
-	re := regexp.MustCompile(`\s+`)
 	processes := make([]Process, 0)
+	name = filepath.Base(name)
 
-	go func() {
-		for {
-			select {
-			case line := <-ch:
-				fields := re.Split(strings.TrimPrefix(line, " "), -1)
-				if len(fields) >= 3 {
-					ps := Process{
-						Pid:  fields[0],
-						PPid: fields[1],
-						Cmd:  strings.Join(fields[2:], " "),
-					}
-					processes = append(processes, ps)
-				}
-			case <-finish:
-				return
-			}
-		}
-	}()
-	err := ExecShellWithNotify(ch, "ps -e -o pid,ppid,cmd | grep '"+name+"'"+" | grep -v 'grep'")
-	finish <- struct{}{}
+	files, err := ioutil.ReadDir("/proc")
 	if err != nil {
-		exitErr, ok := err.(*exec.ExitError)
-		if ok && exitErr.ExitCode() == 1 {
-			return processes, nil
-		}
 		return nil, err
 	}
 
+	for _, file := range files {
+		if !file.IsDir() {
+			continue
+		}
+		pid := file.Name()
+
+		// Read the process command
+		cmdPath := filepath.Join("/proc", file.Name(), "cmdline")
+		cmdBytes, err := ioutil.ReadFile(cmdPath)
+		if err != nil {
+			continue
+		}
+
+		cmd := string(cmdBytes)
+		if strings.Contains(cmd, name) {
+			ppid, err := getParentPid(pid)
+			if err == nil {
+				processes = append(processes, Process{
+					Pid:  file.Name(),
+					PPid: ppid,
+					Cmd:  cmd,
+				})
+			}
+		}
+	}
 	return processes, nil
+}
+
+func getParentPid(pid string) (string, error) {
+	statPath := filepath.Join("/proc", pid, "stat")
+	statBytes, err := ioutil.ReadFile(statPath)
+	if err != nil {
+		return "", err
+	}
+
+	fields := strings.Fields(string(statBytes))
+	if len(fields) < 4 {
+		return "", fmt.Errorf("invalid stat file for pid %s", pid)
+	}
+
+	return fields[3], nil
 }
